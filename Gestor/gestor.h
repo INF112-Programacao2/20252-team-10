3559d14f65c1e7e67d2f6c43b7b721ddb1542ef5
Marcus-v-Prado <marcus.v.prado@ufv.br>
2025-11-20 23:02:29 -0300

    implementacoes adicionadas

--- Gestor/gestor.h
+++ Gestor/gestor.h
@@ -7,70 +7,77 @@
 #include <mysql-cppconn/mysqlx/xdevapi.h>
 #include "../Reagente/reagente.h"
 #include "../PosGraduacao/posgraduacao.h"
-#include "../Estudante/estudante.h"
-
 
 using namespace mysqlx;
 
 class Gestor : public Usuario {
-    private:
+private:
     Laboratorio * laboratorio; // O laboratorio que este Gestor gerencia
-    public:
-        //Construtor
-        Gestor(std::string nome, std::string email, std::string senha, int nivelAcesso, Schema* db);
-        // Destrutor
-        ~Gestor();
+    
+public:
+    //Construtor
+    Gestor(std::string nome, std::string email, std::string senha, int nivelAcesso, Schema* db);
+    
+    // Destrutor
+    ~Gestor();
 
-        static Usuario** usuariosCarregados;
-        static int quantidadeUsuarios;
-        static int capacidadeUsuarios;
+    // Variáveis estáticas
+    static Usuario** usuariosCarregados;
+    static int quantidadeUsuarios;
+    static int capacidadeUsuarios;
 
-        static Gestor** gestores;
-        static int quantidadeGestores;
-        static int capacidadeGestores;
+    static Gestor** gestores;
+    static int quantidadeGestores;
+    static int capacidadeGestores;
 
-        static Estudante** estudantes;
-        static int quantidadeEstudantes;
-        static int capacidadeEstudantes;
+    static Estudante** estudantes;
+    static int quantidadeEstudantes;
+    static int capacidadeEstudantes;
 
-        static PosGraduacao** posGraduandos;
-        static int quantidadePos;
-        static int capacidadePos;
-        //Getters
-        //Retorna o laboratório que o gestor está alocado
-        Laboratorio* getLaboratorio();
-        //Demais funções
-        void cadastrarUsuario();
-        void deletarUsuario();
-        void listarUsuarios();
-        void associarLaboratorio();
-        void associarEstudanteAoLaboratorio(Estudante* estudante, int idLaboratorio, const std::string& papel);
-        void carregarUsuarios();
-        
-        //Menu principal de gerenciamento do laboratório
-        void gerenciarLaboratorio();
+    static PosGraduacao** posGraduandos;
+    static int quantidadePos;
+    static int capacidadePos;
 
-        //Funcionalidades do Menu
-        void listarReagentesDoLaboratorio();
-        void editarReagente();
-        void excluirReagente();
-        void filtrarReagentes();
+    //Getters
+    Laboratorio* getLaboratorio();
 
-        //sets
-        void setLaboratorio(Laboratorio* lab);
-        //cadastra um novo reagente no laboratorio gerenciado por este gestor
-        void cadastrarReagente();
-        //O Gestor tem acesso total
-        void acessarReagentesAlerta();
-        void listarReagentesRestritos();
-        void menuReagentesRestritos();
-        void acessarReagenteRestrito(int idReagente) override;
-        bool estaAssociado() const;
-        // Método no qual o gestor pode geranciar seu laboratório como:
-        // Cadastrar, listar, editar, filtrar e retirar reagente
-        // Além de litar, associar e desassociar estudante
-        // Acessar reagentes em alerta e restrito
-        void gerenciarLaboratório();
+    //Demais funções
+    void cadastrarUsuario();
+    void deletarUsuario();
+    void listarUsuarios();
+    void associarLaboratorio();
+    void associarEstudanteAoLaboratorio(Estudante* estudante, int idLaboratorio, const std::string& papel);
+    void carregarUsuarios();
+    void retirarReagente();
+    void historicoRetiradas();
+    void sairLaboratorio();
+    void desassociarEstudantes();
+    void listarReagentesDoLaboratorio();
+    void editarReagente();
+    void excluirReagente();
+    void filtrarReagentes();
+    
+    // Menu principal de gerenciamento do laboratório
+    void gerenciarLaboratorio();
+
+    //sets
+    void setLaboratorio(Laboratorio* lab);
+    
+    //cadastra um novo reagente no laboratorio gerenciado por este gestor
+    void cadastrarReagente();
+    
+    //O Gestor tem acesso total
+    void acessarReagentesAlerta();
+    void listarReagentesRestritos();
+    void menuReagentesRestritos();
+    void acessarReagenteRestrito(int idReagente) override;
+    bool estaAssociado() const;
+    void listarEstudantes();
+    void associarEstudantes();
+    void cadastrarEstudante();
+    void cadastrarGestor();
+    void deletarGestor();
+    void deletarEstudante();
 };
 
 #endif
